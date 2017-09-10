3ccf0ba11f739fec7d3adb281ea7c4d7da5eb2d1
Tim Blankers <timblankers@gmail.com>
2017-09-10 21:24:39 +0200

    Implement REST calls to RasPi

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule, ErrorHandler } from '@angular/core';
+import { HttpModule }    from '@angular/http';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -21,6 +22,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
--- app/src/pages/lamps/lamps.ts
+++ app/src/pages/lamps/lamps.ts
@@ -1,5 +1,17 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Http, Response, Headers } from '@angular/http';
+import 'rxjs/add/operator/map'
+
+export class Lamp {
+  id: number;
+}
+
+const LAMPS: Lamp[] = [
+  { id: 1 },
+  { id: 2 },
+  { id: 3 },
+];
 
 @Component({
   selector: 'page-lamps',
@@ -7,8 +19,35 @@ import { NavController } from 'ionic-angular';
 })
 export class LampsPage {
 
-  constructor(public navCtrl: NavController) {
+  lamps = LAMPS;
+
+  constructor(public navCtrl: NavController, private _http: Http) {
 
   }
 
+  triggerLamp(lampId, action) {
+    if (action == "on") {
+      return this._http.get('http://192.168.0.106:3000/turnOn/'+lampId)
+      .map((res: Response) => res.json())
+       .subscribe(data => {
+              console.log(data);
+      });
+    } else {
+      return this._http.get('http://192.168.0.106:3000/turnOff/'+lampId)
+      .map((res: Response) => res.json())
+       .subscribe(data => {
+              console.log(data);
+      });
+    }
+  }
+
+  triggerAll(action) {
+    this.lamps.forEach(lamp => {
+      if (action == "on") {
+        this.triggerLamp(lamp.id, "on");
+      } else {
+        this.triggerLamp(lamp.id, "off");        
+      }
+    }) 
+  }
 }
