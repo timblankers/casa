f5d41755c69bde230274d545f1da3358c54792a6
Tim Blankers <timblankers@gmail.com>
2017-09-10 19:33:56 +0200

    Redo Page structure

--- app/src/pages/camera/camera.ts
+++ app/src/pages/camera/camera.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-about',
-  templateUrl: 'about.html'
+  selector: 'page-camera',
+  templateUrl: 'camera.html'
 })
-export class AboutPage {
+export class CameraPage {
 
   constructor(public navCtrl: NavController) {
 
--- app/src/pages/lamps/lamps.ts
+++ app/src/pages/lamps/lamps.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-lamps',
+  templateUrl: 'lamps.html'
 })
-export class ContactPage {
+export class LampsPage {
 
   constructor(public navCtrl: NavController) {
 
